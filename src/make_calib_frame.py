8a214558470ba63e9958c6ca073eb6f04f863acd
janmejoysarkar <janmejoysarker@gmail.com>
2026-07-23 18:52:10 +0200

    No image alignment

--- src/make_calib_frame.py
+++ src/make_calib_frame.py
@@ -25,65 +25,14 @@ from scipy.optimize import curve_fit
 from astropy.coordinates import SkyCoord
 from astropy.convolution import convolve, Box2DKernel
 
-def ComputePixelDistances(image, center_x,center_y):
-    num_rows, num_columns = image.shape
-    distances = np.zeros((num_rows, num_columns))
-    for y in range(num_rows):
-        for x in range(num_columns):
-            distances[y, x] = np.sqrt((x- center_x)**2 +(y - center_y)**2)
-    return distances
-
-def SuitMedianLimbDarkening(image,radius_in_pixel,distance_map,block_size,mu_limit=0.0000001):
-    # this is mainly to calculate QS std from continuum 
-    flat_image = image.flatten()
-    flat_distances = distance_map.flatten()
-    # Combine distances with pixel values
-    pixel_data = np.column_stack((flat_distances,
-    flat_image)) # taking the top left pixel and moving rightwards
-    # Sort pixels by distance
-    sorted_pixel_data = pixel_data[np.argsort(pixel_data[:,
-    0])] 
-    num_pixels = sorted_pixel_data.shape[0]
-    blocks = []
-    number_of_points_in_block=[]
-    for start in range(0, num_pixels, block_size):
-        end = min(start +
-        block_size, num_pixels)
-        block = sorted_pixel_data[start:end]
-        blocks.append(block)
-        number_of_points_in_block.append(len(block))
-    final_mean_distance=[]
-    final_value_signal=[]
-    points_in_blocks_after_conver=[]
-    for block in blocks:
-        # Extract distances and values from the block
-        distances_block = block[:, 0]
-        values_block = block[:, 1]
-        final_mean_distance.append(np.nanmedian(distances_block))
-        final_value_signal.append(np.nanmedian(values_block))
-    R_sun=radius_in_pixel*(np.ones(len(final_mean_distance)))
-    mu=np.sqrt(((R_sun**2)-np.array(final_mean_distance)**2)/(R_sun**2))
-    drop_low_mu_mask=mu>mu_limit
-    mu_drop_last_bin=mu[drop_low_mu_mask]
-    final_value_signal_drop_last_bin=np.array(final_value_signal)[drop_low_mu_mask]
-    return mu_drop_last_bin, final_value_signal_drop_last_bin
-
-def poly_5(x, a, b,c,d,e,f):
-    return (a*(x**5)) + (b*(x**4)) + (c*(x**3)) + (d*(x**2)) + (e*x) + f
-
-def ComputeMu(image,center_x,center_y,radius):
-    distances=ComputePixelDistances(image, center_x,center_y)
-    mu_image=np.sqrt(((radius**2)-(distances)**2)/(radius**2))
-    return mu_image
-
 def openfits(file):
     with fits.open(file) as hdu:
         data= hdu[0].data
         header= hdu[0].header
         return data, header
-def plot_img(img):
+def plot_img(img, vmn= None, vmx= None):
     plt.figure()
-    plt.imshow(img, origin='lower')
+    plt.imshow(img, origin='lower', vmin= vmn, vmax= vmx)
     plt.show()
 
 if __name__=="__main__":
@@ -97,16 +46,9 @@ if __name__=="__main__":
     flat= np.nan_to_num(flat, nan=1.0)
     # Open sun images
     seq = Map(files, sequence=True)
-    cropped_seq, cropped_data=[], []
-    s= int(1200/0.7) # Crop box in pixels (arcsec/platescale)
-    # Centering sun and flat fielding all sun images
-    for m in seq:
-        cpx1, cpx2= int(m.meta['CRPIX1']), int(m.meta['CRPIX2'])
-        cropped_flat= flat[cpx2-s:cpx2+s, cpx1-s:cpx1+s]
-        cropped= m.data[cpx2-s:cpx2+s, cpx1-s:cpx1+s]
-        cropped_data.append(cropped/cropped_flat)
+    img_datas=[m.data for m in seq]
     # Removing small scale structures
-    med= np.nanmedian(cropped_data, axis=0)
+    med= np.nanmedian(img_datas, axis=0)
     kernel= Box2DKernel(15) # Smooting size (larger value >> More aggressive)
     smooth= convolve(med, kernel, boundary='wrap')
     # Generating headers for calib frame
@@ -114,16 +56,13 @@ if __name__=="__main__":
     cpx1, cpx2= int(header['NAXIS1']/2), int(header['NAXIS2']/2)
     header['CRPIX1']= cpx1
     header['CRPIX2']= cpx2
-    # Padding smooth sun to 4k
-    smooth_4k= np.ones((header['NAXIS1'], header['NAXIS2']))
-    smooth_4k[cpx2-s:cpx2+s, cpx1-s:cpx1+s]= smooth
-    # Removing limb darkening from smooth sun 
-    rad=header['R_SUN']
-    dist_map= ComputePixelDistances(smooth_4k, cpx1, cpx2)
-    mu, last_bin= SuitMedianLimbDarkening(smooth_4k, rad, dist_map, 2500, mu_limit= 0.01)
-    coeff, covar= curve_fit(poly_5, mu, last_bin)
-    mu_vals= ComputeMu(smooth_4k, cpx1, cpx2, rad)
-    fit_surface= poly_5(mu_vals, *coeff)
-    limb_dkr_corrected= smooth_4k/fit_surface
     # Writing calib frame
-    fits.writeto(savepath, limb_dkr_corrected, header=header, overwrite=True)
+    fits.writeto(savepath, smooth, header=header, overwrite=True)
+    #visuals
+    fig, ax= plt.subplots(1,3, sharex=True, sharey=True)
+    ax[0].imshow(img_datas[0], origin='lower')
+    ax[1].imshow(smooth, origin='lower')
+    ax[2].imshow(img_datas[0]/smooth, origin='lower', vmin=0.9, vmax=1.1)
+    plt.show()
+
+
